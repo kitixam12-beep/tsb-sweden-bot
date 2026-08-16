a24f28447a9d56fd9cab996628585902e65613e6
kitixam12-beep <kitixam12@gmail.com>
2026-08-16 16:32:26 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -907,27 +907,6 @@ async def send(
 
 @bot.command(name="Tsbswelb")
 async def leaderboard(ctx):
-    # Optional: Delete the user's command message so only the clean bot message stays
-    try:
-        await ctx.message.delete()
-    except:
-        pass
-
-    text = (
-        "### ┌「 Leaderboard 」┐\n"
-        "↳ ✦ **`[ Top 1. ]`** - <@1450925960093503544> — 2 high strong\n"
-        "↳ ✦ **`[ Top 2. ]`** - <@1198641113385410584> — 2 high strong\n"
-        "↳ ✦ **`[ Top 3. ]`** - <@777227138218328085> — 2 high strong\n"
-        "↳ ✦ **`[ Top 4. ]`** - <@954479020811091979> — 2 high strong\n"
-        "↳ ✦ **`[ Top 5. ]`** - <@1358472975015477290> — 2 high stable\n"
-        "↳ ✦ **`[ Top 6. ]`** - <@1332851353311121600> — 2 high weak\n"
-        "↳ ✦ **`[ Top 7. ]`** - <@1067440028344660009> — 2 mid strong\n"
-        "↳ ✦ **`[ Top 8. ]`** - <@1056684144740012094> — 2 mid strong\n"
-        "↳ ✦ **`[ Top 9. ]`** - 「 VACANT 」\n"
-        "↳ ✦ **`[ Top 10. ]`** - 「 VACANT 」\n"
-        "└───────────────────────┘"
-@bot.command(name="Tsbswelb")
-async def leaderboard(ctx):
     try:
         await ctx.message.delete()
     except:
